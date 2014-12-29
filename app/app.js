19b6fc82bf52c5e4252947350bbc8e48d021798f
Gerald A. Wakefield <gwake1@gmail.com>
2014-12-29 16:48:09 -0600

    Option 2 adding controller to module with dependency injection

--- app/app.js
+++ app/app.js
@@ -1,1 +1,5 @@
-var app = angular.module("customersApp", [])
+(function() {
+  "use strict";
+  var app = angular.module("customersApp", [])
+  // even if we assign a variable name here, within the IIFE it is no longer in the global scope; we no longer have access to the variable app through the global name space so we will call it through the module for injecting dependencies with associated controllers.
+}());
--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -1,10 +1,25 @@
 //Option 1
-app.controller("CustomersController", function CustomersController() {
-  this.sortBy = "name";
-  this.reverse = false;
-  this.customers = [ { name: "John", city:"Nashville", joined: "2012/01/15", orderTotal: 9.998 }, { name:"Gerald", city:"Heaven", joined: "1989/08/07", orderTotal: 7.00 }, { name: "Joe", city:"Dirte", joined: "2012/01/23", orderTotal: 14.23 } ];
-  this.doSort = function(propName) {
-    this.sortBy = propName;
-    this.reverse = !this.reverse
-  }
-});
+// app.controller("CustomersController", function CustomersController() {
+//   this.sortBy = "name";
+//   this.reverse = false;
+//   this.customers = [ { name: "John", city:"Nashville", joined: "2012/01/15", orderTotal: 9.998 }, { name:"Gerald", city:"Heaven", joined: "1989/08/07", orderTotal: 7.00 }, { name: "Joe", city:"Dirte", joined: "2012/01/23", orderTotal: 14.23 } ];
+//   this.doSort = function(propName) {
+//     this.sortBy = propName;
+//     this.reverse = !this.reverse
+//   }
+// });
+
+//Option 2
+(function() {
+  "use strict";
+  angular.module("customersApp")
+  .controller("CustomersController", function CustomersController() {
+    this.sortBy = "name";
+    this.reverse = false;
+    this.customers = [ { name: "John", city:"Nashville", joined: "2012/01/15", orderTotal: 9.998 }, { name:"Gerald", city:"Heaven", joined: "1989/08/07", orderTotal: 7.00 }, { name: "Joe", city:"Dirte", joined: "2012/01/23", orderTotal: 14.23 } ];
+    this.doSort = function(propName) {
+      this.sortBy = propName;
+      this.reverse = !this.reverse
+    }
+  });
+}());
