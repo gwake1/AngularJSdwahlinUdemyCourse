be82354660e59e82d83358cc059a942998ca5d32
Gerald A. Wakefield <gwake1@gmail.com>
2014-12-29 17:00:13 -0600

    Even Better minification syntax for readability

--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -1,7 +1,8 @@
 //Option 3
 (function() {
   "use strict";
-  var CustomersController = function CustomersController() {
+  CustomersController.$inject = [ "$scope" ];
+  function CustomersController($scope) {
     this.sortBy = "name";
     this.reverse = false;
     this.customers = [ { name: "John", city:"Nashville", joined: "2012/01/15", orderTotal: 9.998 }, { name:"Gerald", city:"Heaven", joined: "1989/08/07", orderTotal: 7.00 }, { name: "Joe", city:"Dirte", joined: "2012/01/23", orderTotal: 14.23 } ];
@@ -10,7 +11,6 @@
       this.reverse = !this.reverse
     }
   }
-  CustomersController.$inject = [ "$scope" ];
   angular.module("customersApp")
   .controller("CustomersController", CustomersController);
 }());
