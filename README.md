9156dad6c57759733892ce50a8a68cda2b751180
Gerald A. Wakefield <gwake1@gmail.com>
2014-12-29 16:00:26 -0600

    Using As syntax

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ Controllers play a critical role in the AngularJS framework. In this module you'
     + 1.  ng-controller
       + data-ng-controller = "controller name"
       + Controller "as" syntax
+        + must refactor JS to use this in place of $scope
+        + in the view must use "as" to alias the contorller and then reference data using object dot notation
     + 2.  route
--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -1,10 +1,10 @@
-function CustomersController($scope) {
-  $scope.sortBy = "name";
-  $scope.reverse = false;
+function CustomersController() {
+  this.sortBy = "name";
+  this.reverse = false;
 
-  $scope.customers = [ { name: "John", city:"Nashville", joined: "2012/01/15", orderTotal: 9.998 }, { name:"Gerald", city:"Heaven", joined: "1989/08/07", orderTotal: 7.00 }, { name: "Joe", city:"Dirte", joined: "2012/01/23", orderTotal: 14.23 } ];
-  $scope.doSort = function(propName) {
-    $scope.sortBy = propName;
-    $scope.reverse = !$scope.reverse
+  this.customers = [ { name: "John", city:"Nashville", joined: "2012/01/15", orderTotal: 9.998 }, { name:"Gerald", city:"Heaven", joined: "1989/08/07", orderTotal: 7.00 }, { name: "Joe", city:"Dirte", joined: "2012/01/23", orderTotal: 14.23 } ];
+  this.doSort = function(propName) {
+    this.sortBy = propName;
+    this.reverse = !this.reverse
   }
 }
