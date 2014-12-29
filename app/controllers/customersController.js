cf5f23d3c787f8616f157adf893fd8bc5e693b06
Gerald A. Wakefield <gwake1@gmail.com>
2014-12-29 16:52:46 -0600

    Option 3 for adding controllers to module with dependency injection

--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -1,3 +1,34 @@
+//Option 3
+(function() {
+  "use strict";
+  var CustomersController = function CustomersController() {
+    this.sortBy = "name";
+    this.reverse = false;
+    this.customers = [ { name: "John", city:"Nashville", joined: "2012/01/15", orderTotal: 9.998 }, { name:"Gerald", city:"Heaven", joined: "1989/08/07", orderTotal: 7.00 }, { name: "Joe", city:"Dirte", joined: "2012/01/23", orderTotal: 14.23 } ];
+    this.doSort = function(propName) {
+      this.sortBy = propName;
+      this.reverse = !this.reverse
+    }
+  }
+  angular.module("customersApp")
+  .controller("CustomersController", CustomersController);
+}());
+
+//Option 2
+// (function() {
+//   "use strict";
+//   angular.module("customersApp")
+//   .controller("CustomersController", function CustomersController() {
+//     this.sortBy = "name";
+//     this.reverse = false;
+//     this.customers = [ { name: "John", city:"Nashville", joined: "2012/01/15", orderTotal: 9.998 }, { name:"Gerald", city:"Heaven", joined: "1989/08/07", orderTotal: 7.00 }, { name: "Joe", city:"Dirte", joined: "2012/01/23", orderTotal: 14.23 } ];
+//     this.doSort = function(propName) {
+//       this.sortBy = propName;
+//       this.reverse = !this.reverse
+//     }
+//   });
+// }());
+
 //Option 1
 // app.controller("CustomersController", function CustomersController() {
 //   this.sortBy = "name";
@@ -8,18 +39,3 @@
 //     this.reverse = !this.reverse
 //   }
 // });
-
-//Option 2
-(function() {
-  "use strict";
-  angular.module("customersApp")
-  .controller("CustomersController", function CustomersController() {
-    this.sortBy = "name";
-    this.reverse = false;
-    this.customers = [ { name: "John", city:"Nashville", joined: "2012/01/15", orderTotal: 9.998 }, { name:"Gerald", city:"Heaven", joined: "1989/08/07", orderTotal: 7.00 }, { name: "Joe", city:"Dirte", joined: "2012/01/23", orderTotal: 14.23 } ];
-    this.doSort = function(propName) {
-      this.sortBy = propName;
-      this.reverse = !this.reverse
-    }
-  });
-}());
