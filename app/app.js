8f4699d90e406c9c2cc6ba55ae71ce69c8022d0a
Gerald A. Wakefield <gwake1@gmail.com>
2015-01-02 12:31:16 -0600

    Module 5

--- app/app.js
+++ app/app.js
@@ -1,13 +1,18 @@
 (function() {
-  "use strict";
-  var app = angular.module("customersApp", [ "ngRoute" ])
-  // even if we assign a variable name here, within the IIFE it is no longer in the global scope; we no longer have access to the variable app through the global name space so we will call it through the module for injecting dependencies with associated controllers.
-  app.config(function($routeProvider) {
-    $routeProvider
-    .when("/", {
-      controller: "CustomersController",
-      templateurl: "app/views/customers.html"
-    })
-    .otherwise({ redirectTo:"/" })
-  })
+    
+    var app = angular.module('customersApp', ['ngRoute']);
+    
+    app.config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'CustomersController',
+                templateUrl: 'app/views/customers.html'
+            })
+            .when('/orders/:customerId', {
+                controller: 'OrdersController',
+                templateUrl: 'app/views/orders.html'
+            })
+            .otherwise( { redirectTo: '/' } );
+    });
+    
 }());
