2b370b01df54c273ad7bfcafc45f39840bb00b56
Gerald A. Wakefield <gwake1@gmail.com>
2015-01-02 13:22:11 -0600

    Refactoring code ino a factory

--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -1,82 +1,39 @@
+//Option 3
 (function() {
-    
-    var CustomersController = function ($scope) {
-        $scope.sortBy = 'name';
-        $scope.reverse = false;
-        
-        $scope.doSort = function(propName) {
-           $scope.sortBy = propName;
-           $scope.reverse = !$scope.reverse;
-        };
-        
-        $scope.customers= [
-            {
-                id: 1, 
-                joined: '2000-12-02', 
-                name:'John', 
-                city:'Chandler', 
-                orderTotal: 9.9956,
-                orders: [
-                    {
-                        id: 1,
-                        product: 'Shoes',
-                        total: 9.9956
-                    }
-                ]
-            }, 
-            {
-                id: 2, 
-                joined: '1965-01-25',
-                name:'Zed', 
-                city:'Las Vegas', 
-                orderTotal: 19.99,
-                orders: [
-                    {
-                        id: 2,
-                        product: 'Baseball',
-                        total: 9.995
-                    },
-                    {
-                        id: 3,
-                        product: 'Bat',
-                        total: 9.995
-                    }
-                ]
-            },
-            {
-                id: 3, 
-                joined: '1944-06-15',
-                name:'Tina', 
-                city:'New York', 
-                orderTotal:44.99,
-                orders: [
-                    {
-                        id: 4,
-                        product: 'Headphones',
-                        total: 44.99
-                    }
-                ]
-            }, 
-            {
-                id: 4, 
-                joined: '1995-03-28',
-                name:'Dave', 
-                city:'Seattle', 
-                orderTotal:101.50,
-                orders: [
-                    {
-                        id: 5,
-                        product: 'Kindle',
-                        total: 101.50
-                    }
-                ]
-            }
-        ];
+  var CustomersController = function($scope) {
+    $scope.sortBy = "name";
+    $scope.reverse = false;
+    $scope.doSort = function(propName) {
+      $scope.sortBy = propName;
+      $scope.reverse = !$scope.reverse;
     };
-    
-    CustomersController.$inject = ['$scope'];
-
-    angular.module('customersApp')
-      .controller('CustomersController', CustomersController);
-    
+  };
+  CustomersController.$inject = [ "$scope" ];
+  angular.module("customersApp")
+  .controller("CustomersController", CustomersController);
 }());
+//Option 2
+// (function() {
+//   "use strict";
+//   angular.module("customersApp")
+//   .controller("CustomersController", function CustomersController() {
+//     this.sortBy = "name";
+//     this.reverse = false;
+//     this.customers = [ { name: "John", city:"Nashville", joined: "2012/01/15", orderTotal: 9.998 }, { name:"Gerald", city:"Heaven", joined: "1989/08/07", orderTotal: 7.00 }, { name: "Joe", city:"Dirte", joined: "2012/01/23", orderTotal: 14.23 } ];
+//     this.doSort = function(propName) {
+//       this.sortBy = propName;
+//       this.reverse = !this.reverse
+//     }
+//   });
+// }());
+
+//Option 1
+// app.controller("CustomersController", function CustomersController() {
+//   this.sortBy = "name";
+//   this.reverse = false;
+//   this.customers = [ { name: "John", city:"Nashville", joined: "2012/01/15", orderTotal: 9.998 }, { name:"Gerald", city:"Heaven", joined: "1989/08/07", orderTotal: 7.00 }, { name: "Joe", city:"Dirte", joined: "2012/01/23", orderTotal: 14.23 } ];
+//   this.doSort = function(propName) {
+//     this.sortBy = propName;
+//     this.reverse = !this.reverse
+//   }
+// });
